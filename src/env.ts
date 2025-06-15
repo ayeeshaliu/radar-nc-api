85a8398d2a844abae31a55c75f416309eab86105
staa99-mono <alfawwaz@mono.co>
2025-06-15 21:46:48 +0300

    feat: configure default jwt options

--- src/env.ts
+++ src/env.ts
@@ -1,12 +1,19 @@
 import * as process from 'node:process';
 
 import { validateEnvVars } from './modules/configuration';
+import { setDefaultEnvVar } from './modules/configuration/util';
 
 // Set all safe public environment variables here
 // that are *very* unlikely to ever change and remain the
 // same regardless of the environment the application is running in
 process.env.TZ = 'UTC';
-process.env.SERVICE_ID = 'radar-nc-api';
+
+const serviceId = 'radar-nc-api';
+
+process.env.SERVICE_ID = serviceId;
+
+setDefaultEnvVar('JWT_ISSUER', serviceId);
+setDefaultEnvVar('JWT_AUDIENCE', serviceId);
 
 // This is run on startup to ensure that all required environment variables are
 // set before the application starts
